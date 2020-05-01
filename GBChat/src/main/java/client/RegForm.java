fa5eb4c5b5102467418b46cd9b60b63fc43bd8ec
Valery Bulekov <muzaccord@gmail.com>
2020-05-01 20:44:33 +0300

    Java 3. Homework #2.

---
+++ GBChat/src/main/java/client/RegForm.java
@@ -0,0 +1,38 @@
+package client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+
+public class RegForm {
+    public TextField login;
+    public TextField pass;
+    public TextField nick;
+    private Network network;
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+
+    public void registration() {
+        String l = login.getText().trim();
+        String p = pass.getText().trim();
+        String n = nick.getText().trim();
+        if (l.equals("")||p.equals("")||n.equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Внимание!");
+            alert.setHeaderText("Все поля обязательны");
+            alert.setContentText("Заполните, пожалуйста, все поля и повторите попытку");
+            alert.show();
+            return;
+        }
+        network.registration(String.format("/reg %s %s %s", l, p, n));
+
+    }
+
+}
